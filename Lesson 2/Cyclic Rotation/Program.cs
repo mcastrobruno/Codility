29615fbba1dd0c2d16cd901a879dd32587a77f42
Teste <teste>
2018-09-17 17:58:06 +0000

    Solved Cyclic Rotation

---
+++ Lesson 2/Cyclic Rotation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Cyclic_Rotation
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            var result = solution(new int[] { 3, 8, 9, 7, 6 }, 3);
+
+            foreach (var n in result)
+                Console.Write(n);
+
+        }
+
+
+        public static int[] solution(int[] A, int K)
+        {
+            while (K > 0)
+            {
+                int aux = -1;
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        aux = A[i];
+                        var lastElement = A[A.Length - 1];
+                        A[i] = lastElement;
+                    }
+                    else
+                    {
+                        int next = A[i];
+                        A[i] = aux;
+                        aux = next;
+                    }
+                }
+                K--;
+            }
+            return A;
+        }
+    }
+}
