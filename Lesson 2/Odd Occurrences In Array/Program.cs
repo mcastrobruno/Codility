be29d24e99603ff669413785cf93a8f253092d18
Teste <teste>
2018-09-17 19:15:42 +0000

    Odd Occurrences in Array

---
+++ Lesson 2/Odd Occurrences In Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Odd_Occurrences_In_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] { 1, 2, 3, 2, 1 };
+
+            var result = solution(new int[] { 1, 2, 3, 2, 1 });
+
+            foreach (var t in a)
+            {
+                Console.WriteLine(Convert.ToString(t,2));
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+
+
+        public static int solution(int[] A)
+        {
+            int unpaired = 0;
+
+            foreach (var i in A)
+            {
+                unpaired ^= i;
+            }
+
+            return unpaired;
+        }
+    }
+}
