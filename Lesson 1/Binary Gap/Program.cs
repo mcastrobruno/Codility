46e86e507adb0e1830a03fb1ba62b9f0a66015f7
Teste <teste>
2018-09-18 13:19:02 +0000

    Binary Gap

---
+++ Lesson 1/Binary Gap/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Binary_Gap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = Solution(1162);
+            Console.WriteLine(result);
+        }
+        // 500 = 1000010100
+
+
+        public static int Solution(int N)
+        {
+            string binaryNumber = Convert.ToString(N, 2);
+
+            int currentGap = 0;
+
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (binaryNumber[i] == '1') //Procura pelo próximo GAP
+                {
+                    int localGap = 0;
+                    for (int j = i + 1; j < binaryNumber.Length; j++)
+                    {
+                        if (binaryNumber[j] == '0')
+                        {
+                            localGap++;
+                        }
+                        else
+                        {
+                            if (localGap > currentGap)
+                                currentGap = localGap;
+
+                            i = j - 1;
+
+                            break;
+                        }
+                    }
+                }
+            }
+            return currentGap;
+        }
+    }
+}
