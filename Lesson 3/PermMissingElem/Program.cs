3dab81ae9cbad95b234fbaaaf5cf597edfb5c9b2
Bruno Castro <mcastrobruno@gmail.com>
2018-09-20 13:41:57 +0000

    Lesson 3 - Permutation Missing Element

---
+++ Lesson 3/PermMissingElem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] A = new int[10] { 1, 4, 2, 3, 7, 9, 10, 6, 8, 0 };
+
+            int missingElement = Solution(A);
+
+            Console.WriteLine($"Missing Elmement {missingElement}");
+        }
+
+
+        static int Solution(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
