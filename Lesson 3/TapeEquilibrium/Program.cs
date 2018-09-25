489962e17d0e2d6e03cc59151b038b491ade880f
Bruno Castro <mcastrobruno@gmail.com>
2018-09-25 19:41:59 +0000

    Changed all solution methods to private

---
+++ Lesson 3/TapeEquilibrium/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TapeEquilibrium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static private int Solution(int[] a, int n)
+        {
+                throw new NotImplementedException();
+        }
+    }
+}
