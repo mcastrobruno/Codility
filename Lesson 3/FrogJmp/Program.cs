a3c395a324cb18d668882c26ff3b1ccec818c74a
Bruno Castro <mcastrobruno@gmail.com>
2018-09-25 19:32:45 +0000

    FrogJmp

---
+++ Lesson 3/FrogJmp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Solution(1, 100, 1);
+
+            Console.WriteLine($"Resultado: {result}");
+        }
+
+        static public int Solution(int X, int Y, int D)
+        {
+            double subtraction = Y-X;
+            double count = (double)(subtraction/D);            
+            return (int)Math.Ceiling(count);
+        }
+    }
+}
