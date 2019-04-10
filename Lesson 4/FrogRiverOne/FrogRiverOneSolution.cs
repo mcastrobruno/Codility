8e48a8a3d9aecad56c8aef3047c3cb7ee24caf35
Bruno Castro <mcastrobruno@gmail.com>
2019-04-09 23:35:45 -0300

    Solved Lesson 4 - Frog River One exercise

---
+++ Lesson 4/FrogRiverOne/FrogRiverOneSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace FrogRiverOne
+{
+    public class FrogRiverOneSolution
+    {
+        public int Solution(int X, int[] A)
+        {
+            if (X > A.Length + 1)
+                return -1;
+
+            int maxElem = A.Max();
+            int[] countingArr = new int[maxElem + 1 > A.Length + 1 ? maxElem + 1 : A.Length + 1];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                bool isComplete = true;
+
+                countingArr[A[i]]++;
+
+                //Check if path is complete
+                for (int j = 1; j <= X; j++)
+                {
+                    if (countingArr[j] == 0)
+                    {
+                        isComplete = false;
+                        break;
+                    }
+                }
+
+                if (isComplete)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
